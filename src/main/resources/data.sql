cb3dd96b73d85e04cd62e49bd5aff3a636bd379d
KamilKowalczyk8 <kamil.kowalczyk9.1@onet.pl>
2023-07-17 19:30:31 +0200

    first commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+
+delete from users;
+insert into users (name, surname, email, city) values
+('Ola','Kowalska','ola@example.com','Warszawa'),
+('adam','Kowalski','adam@example.com','krakow'),
+('kasia','niwak','kasia@example.com','gdynia');
